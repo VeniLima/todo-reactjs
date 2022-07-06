d382277731e43f79ebf84fa45038afa26cafb14e
Vinicius Lima <vinicius.lima.brasil1@gmail.com>
2022-07-06 18:42:15 -0300

    application core

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,29 @@ import "./styles/global.css";
 import styles from "./styles/App.module.css";
 import { Header } from "./components/Header";
 import { PlusCircle } from "phosphor-react";
+import { Main } from "./components/Main";
+import { useState } from "react";
 
 function App() {
+  const [NewTask, setNewTask] = useState<string[]>([""]);
+
+  function handleNewTask(event: any) {
+    event.preventDefault();
+    setNewTask([...NewTask, event.target.value]);
+  }
+
   return (
     <div>
       <Header />
       <div>
-        <form className={styles.form}>
+        <form onSubmit={handleNewTask} className={styles.form}>
           <input placeholder="Adicione uma nova tarefa" />
           <button type="submit">
             Criar <PlusCircle />
           </button>
         </form>
       </div>
-      <div>
-        <span>Tarefas criadas</span> <span>0</span>
-        <span>Concluídas</span> <span>0</span>
-      </div>
+      <Main />
     </div>
   );
 }
--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,7 +1,20 @@
-export function Task() {
+import { Trash } from "phosphor-react";
+import styles from "../styles/Task.module.css";
+
+interface TaskProps {
+  content: string;
+}
+
+export function Task(props: TaskProps) {
   return (
     <div>
-      <h1>task</h1>
+      <div className={styles.task}>
+        <input className={styles.checkboxRound} type="checkbox" />
+        <p className={styles.taskText}>{props.content}</p>
+        <button>
+          <Trash size={24} />
+        </button>
+      </div>
     </div>
   );
 }
